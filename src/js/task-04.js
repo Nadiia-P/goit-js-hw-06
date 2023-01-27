9aeb29df410e8a9c5ebd062f007847e0dbf38a72
Nadiia P <nadiia.parakhina@gmail.com>
2023-01-27 18:44:17 +0200

    task#5 is done

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -3,7 +3,6 @@ const incrButton = document.querySelector('[data-action="increment"]');
 const decrButton = document.querySelector('[data-action="decrement"]');
 
 let counterValue = 0;
-console.log(counterValue);
 
 incrButton.addEventListener('click', () => {
 counterValue += 1;
@@ -15,4 +14,3 @@ counterValue -= 1;
 counterContainer.textContent = counterValue;
 })
 
-console.log(counterContainer.innerText);
