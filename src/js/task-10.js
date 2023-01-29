d0527d99a8f2015d6f063a10821f538d0a3c1f52
Nadiia P <nadiia.parakhina@gmail.com>
2023-01-29 19:30:47 +0200

    task#10 is done

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  // return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const arrayOfColorFunctions = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+  let randomColorString = '#';
+  for (let i = 0; i < 6; i++){
+    let index = Math.floor(Math.random() * 16)
+    let value = arrayOfColorFunctions[index]
+    randomColorString += value;
+  }
+  return randomColorString
 }
 
 const refs = {
