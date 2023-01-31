f57b7106acea43f0018854eb6813743a3b506291
Nadiia-P <111305069+Nadiia-P@users.noreply.github.com>
2023-01-31 21:28:17 +0200

    Delete src directory

--- src/js/task-01.js
+++
@@ -1,9 +0,0 @@
-const allCategoriesArr = document.querySelectorAll('.item');
-console.log(`Number of categories: ${allCategoriesArr.length}`);
-
-
-allCategoriesArr.forEach((category) => {
-  const categoryTitleText = category.querySelector('h2').textContent;
-  const categoryItemsQuantity = category.querySelectorAll('li').length;
-  console.log(`\nCategory: ${categoryTitleText}\nElements: ${categoryItemsQuantity}`);
-})
